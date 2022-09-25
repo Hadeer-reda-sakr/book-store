c69cef4f988dd0a4d47c4dc5e9b56aacbff92630
Hadeer-reda-sakr <60269940+Hadeer-reda-sakr@users.noreply.github.com>
2022-09-25 22:13:32 +0200

    Add files via upload

---
+++ book.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mvc_mine.Models
+{
+    public class book
+    {
+        [Display (Name =" Book Title")]
+        [MaxLength(8) , ErrorMessage("the length must be between 8 to 5 letters") , MinLength(5) ]
+        [Required]
+        public string Title { get; set; }
+        [Display(Name ="Book Price")]
+        public int price { get; set; }
+        [Display(Name ="Authors: ")]
+        public string author { get; set; }
+        [Display(Name = "Description: ")]
+        public string description { get; set; }
+
+
+    }
+}
---
+++ bookController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mvc_mine.Models;
+using System.Collections.Generic;
+
+namespace mvc_mine.Controllers
+{
+    public class bookController : Controller
+
+    {
+        List<book> books = new List<book>() {
+                new book(){Title ="Asp.net " , price = 50, author = "mohamed"  ,
+                    description= "for beginners easy explaination of Asp.net that you find your self inside joureny never want to end"} ,
+                new book (){Title = "c#" , price = 880,author="ahmed & gorge" , 
+                    description= "for beginners easy explaination of C# that you find your self inside joureny never want to end" },
+                new book(){Title ="SQl " , price = 900, author = "ali", description = "for beginners easy explaination of SQL that you find your self inside joureny never want to end" } ,
+
+            };
+
+        public IActionResult index()
+        {
+
+            return View(books);
+        }
+
+        public IActionResult detalis()
+        {
+
+            return View();
+        }
+    }
+}
